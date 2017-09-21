883777b4f1e691b6d73ad5e43deb61c1829338e1
callertrain <cahcardeal@gmail.com>
2017-09-21 15:37:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var uppercase = "HELLO"
-var lowercase = "hello"
-var mixedCase = "Hi there"
-var spy
-
 function shout(string){
   return string.toUpperCase();
 }
@@ -12,11 +7,5 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log(string.toUpperCase);
-}
-
-function logWhisper(string){
-  if (`string` === 'HELLO') {
-  console.log(`string`.toLowerCase);
-  }
+  console.log(string.toUpperCase());
 }

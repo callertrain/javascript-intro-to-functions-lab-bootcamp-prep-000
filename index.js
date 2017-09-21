5177e6d2600113da6caaa6a0b276880259e7187f
callertrain <cahcardeal@gmail.com>
2017-09-21 15:43:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string)
+{
   if (string === toLowerCase()){
     return "I can\'t hear you!";
   }

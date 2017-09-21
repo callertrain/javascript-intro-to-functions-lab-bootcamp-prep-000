87bf2a182237587466b2ed6b043133ab4dd7737b
callertrain <cahcardeal@gmail.com>
2017-09-21 15:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string){
+  if (`string` === lowercase){
+    return "I can\'t hear you!";
+  }
+
+  if else (`string` === uppercase){
+    return "YES INDEED";
+  }
+
+  else (`string` === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}

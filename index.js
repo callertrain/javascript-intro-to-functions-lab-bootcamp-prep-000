2eb15ee23a0d9df4039fa83858316c2299decdf0
callertrain <cahcardeal@gmail.com>
2017-09-21 15:46:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string)
     return "YES INDEED";
   }
 
-  if (string === "I love you, Grandma."){
+  if ("I love you, Grandma." === string){
     return "I love you, too."
   }
 }
